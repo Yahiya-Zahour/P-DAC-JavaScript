b77102f6c4805d44258922c4939075f5eaa6c250
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-09-13 17:50:23 +0530

    practice

---
+++ Ifelse.js
@@ -0,0 +1,19 @@
+// const age =11;
+// const age1 =18;
+// if (age>=18){
+// console.log("your are eligible for driving licence  😍😍😍")
+// }
+// else{
+//   console.log ("your are not   eligible for driving licence 😥😣😥")
+// }
+const ageOne =111;
+const ageFive =17;
+const minAge =18;
+if(ageOne >=minAge){
+    console.log("i am Adult");
+}else if (ageFive ===17){
+    console.log("i am  not child");
+}else{
+    console.log("i dont know");
+}
+
